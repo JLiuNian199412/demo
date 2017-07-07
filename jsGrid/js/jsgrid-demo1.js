5ace3a92dfc14cd24267581e614763f51059ed0c
jiziqiang <jiziqiang@eshippinggateway.com>
2017-07-07 09:43:40 +0800

    项目

---
+++ jsGrid/js/jsgrid-demo1.js
@@ -0,0 +1,126 @@
+/**
+ * Created by zhu zhu on 2016/12/16.
+ */
+var clients = [
+    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },{ "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
+    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
+];
+
+var countries = [
+    { Name: "", Id: 0 },
+    { Name: "United States", Id: 1 },
+    { Name: "Canada", Id: 2 },
+    { Name: "United Kingdom", Id: 3 }
+];
+
+var client = [];
+
+$("#jsGrid").jsGrid({
+
+    controller:{
+        //loadData: function(filter){
+        //    return $.ajax({
+        //        type: "get",
+        //        url: "items",
+        //        data: filter
+        //    });
+        //},
+        //insertItem: function(item){
+        //    return $.ajax({
+        //        type: "post",
+        //        url: "items",
+        //        data: item
+        //    });
+        //},
+        updateItem: function(item){
+            //return $.ajax({
+            //    type: "put",
+            //    url: "items",
+            //    data: item
+            //});
+            console.log(item)
+            if(item)
+            console.log(clients[0])
+        }
+        //,
+        //deleteItem: function(item){
+        //    return $.ajax({
+        //        type: "delete",
+        //        url: "items",
+        //        data: item
+        //    });
+        //}
+    },
+
+    width: "100%",
+    //height: "100%",
+
+    //heading:true, //是否显示表头 默认为true
+    //inserting: true,//是否启动插入 默认为false
+    //filtering:true,//显示查找组件 默认为false
+    editing: true,// 能否编辑 默认为false
+    selecting:false,
+    sorting: true,
+    paging: true,
+    noDataContent: "没有合适的数据，请更改条件后再试",
+
+    confirmDeleting: true,
+    deleteConfirm: "确定要删除么?",
+
+    pagerContainer:null,
+    pageIndex: 1,
+    pageSize: 6,//每页显示的数量
+    pageButtonCount: 7,//最多显示多少个页码
+    pagerFormat: "Pages: {first} {prev} {pages} {next} {last} 总共 {pageCount} 页", //占位符{}，支持的占位如实例 ,
+    pagePrevText: "上一页",
+    pageNextText: "下一页",
+    pageFirstText: "首页",
+    pageLastText: "末页",
+
+    data: clients,
+
+    fields: [
+        { name: "Name", type: "text", width: 150,readOnly:true,validate: "required",css:"one" },
+        { name: "Age", type: "number",sorter:"number", width: 50,align:'center' },
+        { name: "Address", type: "text", width: 200 },
+        { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
+        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+        { type: "control" }
+    ]
+});
+$(".one").click(function(){
+   console.log(1111);
+    if (window.event.keyCode==13){
+        console.log(22222)
+    }
+});
